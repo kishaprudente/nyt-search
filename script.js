d416a52c5b07634b17235e5b71d2a78c5906f0e6
Jiyoon Kim <jiyoon9886@gmail.com>
2020-04-14 13:10:42 -0400

    added jquery links and fixed api key

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-var apiKey = hOb2YoAGqGjlVcEJcW8tLTjxatGq4VhA
+var apiKey = "hOb2YoAGqGjlVcEJcW8tLTjxatGq4VhA";
 var queryURL = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=" + apiKey;
+
+console.log(queryURL);
