7b956bd017854db08f668e43281906bc0a1a8254
Kisha Prudente <kisha.prudente@gmail.com>
2020-04-14 12:56:27 -0400

    added the form, article card and footer

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-var apiKey = hOb2YoAGqGjlVcEJcW8tLTjxatGq4VhA
-var queryURL = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=" + apiKey;
+var apiKey = hOb2YoAGqGjlVcEJcW8tLTjxatGq4VhA;
+var queryURL =
+  "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=" + apiKey;
